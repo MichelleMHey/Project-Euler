c11616ec7933aa76dcfbad3e4ee0d5eb406250ff
MichelleMHey <michellemhey@gmail.com>
2013-05-12 16:44:10 -0500

    Solutions to project Euler problems

--- p2.rb
+++ p2.rb
@@ -2,12 +2,9 @@ sum = 0
 a = 0
 b = 1
 while b <= 4000000
-  c = a + b #find the next term
-  a = b #shift the terms up
-  b = c #shift the terms up
-
-  #ruby trick to do the same thing
-  #a, b = b, a + b
+  c = a + b 
+  a = b 
+  b = c 
   if b % 2 == 0
     sum += b
   end
